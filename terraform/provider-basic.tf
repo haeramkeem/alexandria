c3bd3f525098c092566e83e78873b98645e1846b
haeramkeem <ewqdsacxz2345@gmail.com>
2022-12-21 00:00:35 +0900

    feat: migrate haeramkeem/terraform-iac

---
+++ terraform/provider-basic.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.17.1"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "admin"
+  region  = "ap-northeast-2"
+}
